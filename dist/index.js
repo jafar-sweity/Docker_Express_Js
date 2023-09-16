5231110881e8e40889d5d4eb312b084219be8345
jafar sweity <jata1518@gmail.com>
2023-09-16 12:51:03 +0300

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+import { log } from 'console';
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.json({ msg: 'Hello from express' });
+});
+const port = 3000;
+app.listen(port, () => {
+    log(`APP run in port ${port}`);
+});
